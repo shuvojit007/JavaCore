e36c33d300e443cca0220ce9166b8c4b429964eb
shuvojit007 <shuvojitkar241@gmail.com>
2018-03-28 02:35:26 +0600

    Add Java 8 Features Stream Api
    1. Map
    2. FlatMap
    3. Filter

--- src/Java_Stream/Stream_Api.java
+++ src/Java_Stream/Stream_Api.java
@@ -2,6 +2,9 @@ package Java_Stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+
+//Link : http://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
 
 public class Stream_Api {
     public static void main(String[] args) {
@@ -14,5 +17,21 @@ public class Stream_Api {
                 .map(String::toUpperCase) //use to transform
                 .sorted()
                 .forEach(System.out::println);
+
+            //===================================
+
+        Arrays.stream(new int[] {1, 2, 3})
+                .map(n -> 2 * n + 1)
+                .average()
+                .ifPresent(System.out::println);
+
+        //===================
+/*        Sometimes it's useful to transform a regular object stream to a primitive stream or vice versa.
+        For that purpose object streams support the special mapping operations mapToInt(), mapToLong() and mapToDouble:*/
+        Stream.of("a1", "a2", "a3")
+                .map(s -> s.substring(1))
+                .mapToInt(Integer::parseInt)
+                .max()
+                .ifPresent(System.out::println);
     }
 }
--- src/Java_Stream/Stream_FlatMap.java
+++ src/Java_Stream/Stream_FlatMap.java
@@ -3,13 +3,15 @@ package Java_Stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Stream_FlatMap {
 
     public static void main(String[] args) {
 
         List<User> users = Arrays.asList(
-                new User("Peter", 20, Arrays.asList("1", "2")),
+                new User("Peter", 20, Arrays.asList("1","5", "2")),
                 new User("Sam", 40, Arrays.asList("3", "4", "5")),
                 new User("Ryan", 60, Arrays.asList("6")),
                 new User("Adam", 70, Arrays.asList("7", "8"))
@@ -18,11 +20,30 @@ public class Stream_FlatMap {
 
         System.out.println("Functional Style: ");
 
+        Optional<String> stringOptional1=users.stream()
+                .map(new Function<User,Stream<String>>() {
+                    @Override
+                    public Stream<String> apply(User user) {
+                        return user.getPhoneNumbers().stream();
+                    }
+                })
+                .flatMap(new Function<Stream<String>, Stream<String>>() {
+                    @Override
+                    public Stream<String> apply(Stream<String> stringStream) {
+                        return stringStream.filter(phn->phn.equals("5"));
+                    }
+                }).findAny();
+        stringOptional1.ifPresent(System.out::println);
+        System.out.println("Method 2");
+
 
         Optional<String> stringOptional = users.stream()
                 .map(user -> user.getPhoneNumbers().stream())
                 .flatMap(stringStream -> stringStream.filter(phoneNo -> phoneNo.equals("10")))
                 .findAny();
+
+        //findAny() another Strem Api...if its find any value then its
+        //stop the stream its return type optional
 
         stringOptional.ifPresent(System.out::println);
 
